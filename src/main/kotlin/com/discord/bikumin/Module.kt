20c1252719b66cd9713299b072751b217b19e480
takagi <iam.takagi.dev@gmail.com>
2021-03-28 02:41:50 +0900

    fix bot by lazy in module

--- src/main/kotlin/com/discord/bikumin/Module.kt
+++ src/main/kotlin/com/discord/bikumin/Module.kt
@@ -5,21 +5,8 @@ import io.ktor.features.*
 import io.ktor.http.*
 
 fun Application.module() {
-
-    install(DefaultHeaders)
-    install(CallLogging)
-
-    install(CORS) {
-        method(HttpMethod.Options)
-        method(HttpMethod.Put)
-        method(HttpMethod.Delete)
-        method(HttpMethod.Post)
-        method(HttpMethod.Patch)
-        header(HttpHeaders.AccessControlAllowHeaders)
-        header(HttpHeaders.ContentType)
-        header(HttpHeaders.AccessControlAllowOrigin)
-        allowCredentials = true
-        anyHost()
+    val bot by lazy {
+        Bot(Env.BOT_TOKEN).start()
     }
 }
 
